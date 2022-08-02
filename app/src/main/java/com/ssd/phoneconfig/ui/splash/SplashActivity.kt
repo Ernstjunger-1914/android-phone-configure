8c7379b4e9b12b951caff8803041a95ffd4d5bd4
손소독 <vot768@naver.com>
2022-08-02 22:18:48 +0900

    1. splash screen 동작하도록 구현(대기 시간을 임의로 조정한 것이기에 추후 수정 필요)

--- app/src/main/java/com/ssd/phoneconfig/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/ssd/phoneconfig/ui/splash/SplashActivity.kt
@@ -1,14 +1,24 @@
 package com.ssd.phoneconfig.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.ssd.phoneconfig.R
 import com.ssd.phoneconfig.base.BaseActivity
+import com.ssd.phoneconfig.ui.main.MainActivity
 
 class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
     }
 
 }
